a21d62ee204fbbaa9e31217ffc6238aedda52055
Sensetivity <notfound.la2.asterios.tm@gmail.com>
2018-11-23 14:49:46 +0200

    Added migration

---
+++ src/migrations/m160716_173208_changelogs.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+class m160716_173208_changelogs extends Migration
+{
+    // Use safeUp/safeDown to run migration code within a transaction
+    private $table = '{{%changelogs}}';
+
+    public function safeUp()
+    {
+        $this->createTable($this->table, [
+            'id' => $this->primaryKey()->unsigned(),
+            'relatedObjectType' => $this->string(191)->notNull(),
+            'relatedObjectId' => $this->integer()->notNull(),
+            'old_data' => $this->text(),
+            'new_data' => $this->text(),
+            'createdAt' => $this->integer(),
+            'type' => $this->string(191)->null(),
+            'userId' => $this->integer(),
+            'hostname' => $this->string(191)
+        ], 'ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci');
+
+        $this->createIndex('IN_related_object_type', $this->table, 'relatedObjectType');
+        $this->createIndex('IN_related_object_id', $this->table, 'relatedObjectId');
+        $this->createIndex('IN_type', $this->table, 'type');
+    }
+
+    public function safeDown()
+    {
+        $this->dropTable($this->table);
+    }
+}
